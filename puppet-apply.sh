990de7d60be343306a379568b698b13203f4a4b6
Dan Gibbons <dan.gibbons@rbi.co.uk>
2017-06-15 12:29:24 +0100

    Fixes

--- puppet-apply.sh
+++ puppet-apply.sh
@@ -1,20 +1,44 @@
 #!/usr/bin/env bash
 
-cat >> /opt/puppetlabs/facter/facts.d/role.sh << EOF
-#!/bin/bash
-echo "role=::role"
-EOF
-chmod +x /opt/puppetlabs/facter/facts.d/role.sh
+#if [[ "$1" =~ ^((-{1,2})([Hh]$|[Hh][Ee][Ll][Pp])|)$ ]]; then
+#    echo 'USAGE: '; exit 1
+#else
+    while [[ $# -gt 0 ]]; do
+      opt="$1"
+      shift;
+      current_arg="$1"
+      if [[ "$current_arg" =~ ^-{1,2}.* ]]; then
+        echo "WARNING: You may have left an argument blank. Double check your command."
+      fi
+      case "$opt" in
+        "-b"|"--build"      ) BUILD=true; shift;;
+        "-d"|"--debug"      ) DEBUG="$1"; shif t;;
+        "-r"|"--role"       ) ROLE="$1"; shift;;
+        *                   ) echo "ERROR: Invalid option: \""$opt"\"" >&2
+                              exit 1;;
+      esac
+    done
+#fi
 
-# Create environment folder if it doesn't exist - puppet apply failure otherwise
-environment=$(awk -F "=" '/environment=/ {print $2}' /etc/puppetlabs/puppet/puppet.conf)
+ROLE_FILE=$"#!/bin/bash
+echo role=$ROLE"
 
-mkdir -p /etc/puppetlabs/code/environments/$environment
-echo "Running puppet apply for environment: $environment"
+if [ "$BUILD" = true ]; then
+    r10k deploy environment -pv
+else
+    echo "$ROLE_FILE" > /opt/puppetlabs/facter/facts.d/role.sh
+    chmod +x /opt/puppetlabs/facter/facts.d/role.sh
 
-puppet config set strict_variables false
-puppet config set hiera_config hiera.yaml
-puppet config set environmentpath environments
-puppet config set basemodulepath environments/$environment/modules:environments/$environment/site
+    # Create environment folder if it doesn't exist - puppet apply failure otherwise
+    environment=$(awk -F "=" '/environment=/ {print $2}' /etc/puppetlabs/puppet/puppet.conf)
 
-puppet apply environments/$environment/manifests/default.pp
+    mkdir -p /etc/puppetlabs/code/environments/$environment
+    echo "Running puppet apply for environment: $environment"
+
+    puppet config set strict_variables false
+    puppet config set hiera_config hiera.yaml
+    puppet config set environmentpath environments
+    puppet config set basemodulepath environments/$environment/modules:environments/$environment/site
+
+    puppet apply environments/$environment/manifests/default.pp
+fi
